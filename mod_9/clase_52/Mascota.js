effda061b798dd51cacb2693e4a0ff21ceef6b37
Luis <100473510+carlos8788@users.noreply.github.com>
2023-11-30 20:16:26 -0300

    clase_52

---
+++ mod_9/clase_52/Mascota.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Mascota extends Component {
+    render() {
+        return (
+            <div>
+                <h1></h1>
+                <img />
+            </div>
+        );
+    };
+}
+
+export default Mascota;
+
+import React, { Component } from 'react';
+
+class Mascota extends Component {
+
+    render() {
+
+        return (
+            <div>
+                <h1></h1>
+                <img></img>
+            </div>
+        )
+
+    }
+
+}
+
+export default Mascota
